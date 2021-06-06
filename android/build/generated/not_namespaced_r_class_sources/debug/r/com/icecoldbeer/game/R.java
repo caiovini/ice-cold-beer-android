fe31c40f338581088cf506c8f61285cea015a813
caiovini <caio2791@yahoo.com.br>
2021-06-05 21:38:02 -0300

    Changes to ic_launcher

--- android/build/generated/not_namespaced_r_class_sources/debug/r/com/icecoldbeer/game/R.java
+++ android/build/generated/not_namespaced_r_class_sources/debug/r/com/icecoldbeer/game/R.java
@@ -15,10 +15,13 @@ public final class R {
     public static final int ic_launcher=0x7f020000;
     public static final int ic_launcher_foreground=0x7f020001;
   }
+  public static final class mipmap {
+    public static final int ic_launcher=0x7f030000;
+  }
   public static final class string {
-    public static final int app_name=0x7f030000;
+    public static final int app_name=0x7f040000;
   }
   public static final class style {
-    public static final int GdxTheme=0x7f040000;
+    public static final int GdxTheme=0x7f050000;
   }
 }
